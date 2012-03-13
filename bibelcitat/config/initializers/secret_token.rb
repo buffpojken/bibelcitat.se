a577b133c1b34e2ec2bc0cd596965f2653ed096f
Daniel Sundström <daniel@sykewarrior.com>
2012-03-13 21:09:20 +0100

    initial commiy

---
+++ bibelcitat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bibelcitat::Application.config.secret_token = '851834296041cbd9a0fd1ea6def310cf389fa1911c4623c2a4300eb7feef3bc8a2fb2e5322fa9700fe38ab191372c2dc96c4bc9aeb7331cc1ea27fe49362d609'
